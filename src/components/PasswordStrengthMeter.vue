f3d0bb9b32db02e9983a9e939c80d7c8638c1289
Kalle Pyörälä <kalle.pyorala@redicom.fi>
2018-09-26 16:42:00 +0200

    small fixes

--- src/components/PasswordStrengthMeter.vue
+++ src/components/PasswordStrengthMeter.vue
@@ -12,6 +12,7 @@
         :placeholder="placeholder"
         :required="required"
         :disabled="disabled"
+        autocomplete="new-password"
       >
       <div class="Password__icons">
         <div
@@ -216,7 +217,7 @@
     },
     data () {
       return {
-        password: null,
+        password: this.value,
         _showPassword: false
       }
     },
@@ -299,149 +300,150 @@
 </script>
 
 <style lang="scss">
-  [v-cloak] {
-    display: none;
-  }
-
-  .Password {
-    max-width:  400px;
-    margin:  0 auto;
-  }
-
-  .Password__group {
-    position: relative;
-  }
-
-  .Password__strength-meter {
-    position: relative;
-    height: 3px;
-    background: #DDD;
-    margin: 10px auto 20px;
-    border-radius: 3px;
+[v-cloak] {
+  display: none;
 }
 
-  .Password__strength-meter:before, .Password__strength-meter:after {
-    content: '';
-    height: inherit;
-    background: transparent;
-    display: block;
-    border-color: #FFF;
-    border-style: solid;
-    border-width: 0 5px 0 5px;
-    position: absolute;
-    width: 80px;
-    z-index: 10;
+.Password {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.Password__group {
+  position: relative;
+}
+
+.Password__strength-meter {
+  position: relative;
+  height: 3px;
+  background: #ddd;
+  margin: 10px auto 20px;
+  border-radius: 3px;
+}
+
+.Password__strength-meter:before,
+.Password__strength-meter:after {
+  content: "";
+  height: inherit;
+  background: transparent;
+  display: block;
+  border-color: #fff;
+  border-style: solid;
+  border-width: 0 5px 0 5px;
+  position: absolute;
+  width: 80px;
+  z-index: 10;
+}
+
+.Password__strength-meter:before {
+  left: 70px;
+}
+
+.Password__strength-meter:after {
+  right: 70px;
+}
+
+.Password__strength-meter--fill {
+  background: transparent;
+  height: inherit;
+  position: absolute;
+  width: 0;
+  border-radius: inherit;
+  transition: width 0.5s ease-in-out, background 0.25s;
+}
+
+.Password__strength-meter--fill[data-score="0"] {
+  background: darkred;
+  width: 20%;
+}
+
+.Password__strength-meter--fill[data-score="1"] {
+  background: orangered;
+  width: 40%;
+}
+
+.Password__strength-meter--fill[data-score="2"] {
+  background: orange;
+  width: 60%;
+}
+
+.Password__strength-meter--fill[data-score="3"] {
+  background: yellowgreen;
+  width: 80%;
+}
+
+.Password__strength-meter--fill[data-score="4"] {
+  background: green;
+  width: 100%;
+}
+
+.Password__field {
+  background-color: #f1f1f1;
+  border: 1px solid #f1f1f1;
+  border-radius: 2px;
+  box-sizing: border-box;
+  font-size: 14px;
+  padding: 13px;
+  width: 100%;
+}
+
+.Password__field--disabled {
+  background-color: #f6f6f6;
+  border: 1px solid #f6f6f6;
+}
+
+.Password__icons {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.Password__toggle {
+  line-height: 1.1;
+  margin-right: 13px;
+}
+
+.Password__badge {
+  position: relative;
+  color: white;
+  border-radius: 6px;
+  padding: 3px;
+  width: 30px;
+  height: 15px;
+  font-size: 14px;
+  line-height: 1.1;
+  margin-right: 13px;
+}
+
+.Password__badge--error {
+  background: red;
+}
+
+.Password__badge--success {
+  background: #1bbf1b;
+}
+
+.btn-clean {
+  appearance: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  color: #777777;
+  padding: 0;
+
+  svg {
+    fill: currentColor;
   }
 
-  .Password__strength-meter:before {
-    left: 70px;
+  &:hover,
+  &:focus {
+    color: #404b69;
   }
-
-  .Password__strength-meter:after {
-    right: 70px;
-  }
-
-  .Password__strength-meter--fill {
-    background: transparent;
-    height: inherit;
-    position: absolute;
-    width: 0;
-    border-radius: inherit;
-    transition: width 0.5s ease-in-out, background 0.25s;
-  }
-
-  .Password__strength-meter--fill[data-score='0'] {
-    background: darkred;
-    width: 20%;
-  }
-
-  .Password__strength-meter--fill[data-score='1'] {
-    background: orangered;
-    width: 40%;
-  }
-
-  .Password__strength-meter--fill[data-score='2'] {
-    background: orange;
-    width: 60%;
-  }
-
-  .Password__strength-meter--fill[data-score='3'] {
-    background: yellowgreen;
-    width: 80%;
-  }
-
-  .Password__strength-meter--fill[data-score='4'] {
-    background: green;
-    width: 100%;
-  }
-
-  .Password__field {
-    background-color: #f1f1f1;
-    border: 1px solid #f1f1f1;
-    border-radius: 2px;
-    box-sizing: border-box;
-    font-size: 14px;
-    padding: 13px;
-    width: 100%;
-  }
-
-  .Password__field--disabled {
-    background-color: #f6f6f6;
-    border: 1px solid #f6f6f6;
-  }
-
-  .Password__icons {
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-  }
-
-  .Password__toggle {
-    line-height: 1.1;
-    margin-right: 13px;
-  }
-
-  .Password__badge {
-    position: relative;
-    color: white;
-    border-radius: 6px;
-    padding: 3px;
-    width: 30px;
-    height: 15px;
-    font-size: 14px;
-    line-height: 1.1;
-    margin-right: 13px;
-  }
-
-  .Password__badge--error {
-    background: red;
-  }
-
-  .Password__badge--success {
-    background: #1bbf1b;
-  }
-
-  .btn-clean {
-    appearance: none;
-    background: none;
-    border: none;
-    cursor: pointer;
-    outline: none;
-    color: #777777;
-    padding: 0;
-
-    svg {
-      fill: currentColor;
-    }
-
-    &:hover, &:focus {
-      color: #404B69;
-    }
-  }
-
+}
 </style>
